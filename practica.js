238c1f87e27bf690e6824000972980784ee8f14d
sofialuini302010 <sofikluini@gmail.com>
2025-08-29 15:26:08 -0300

    Add files via upload

---
+++ practica.js
@@ -0,0 +1,5 @@
+//Declaracion de variables
+let boton = document.getElementById("btntexto");
+let parrafo = document.getElementById("texto");
+//Funcion para modificar el parrafo
+boton.addEventListener("click",function()
